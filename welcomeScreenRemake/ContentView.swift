9665791e0a96493901103a2ed21396557e86b004
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-12-27 15:06:58 -0600

    Initial Commit

---
+++ welcomeScreenRemake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  welcomeScreenRemake
+//
+//  Created by Justin Hold on 12/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ welcomeScreenRemake/welcomeScreenRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  welcomeScreenRemakeApp.swift
+//  welcomeScreenRemake
+//
+//  Created by Justin Hold on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct welcomeScreenRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
